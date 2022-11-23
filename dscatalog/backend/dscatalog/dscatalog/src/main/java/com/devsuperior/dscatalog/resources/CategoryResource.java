957f2a0d84ffa03cec5173673c47ac8802c09204
Marcos Vinício Fernandes <marcosviniciohd@hotmail.com>
2022-11-22 21:25:09 -0300

    Find Category by Id, exception handling

--- dscatalog/backend/dscatalog/dscatalog/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ dscatalog/backend/dscatalog/dscatalog/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -2,9 +2,11 @@ package com.devsuperior.dscatalog.resources;
 
 import java.util.List;
 
+import com.devsuperior.dscatalog.dto.CategoryDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,9 +21,16 @@ public class CategoryResource {
 	private CategoryService service;
 	
 	@GetMapping
-	public ResponseEntity<List<Category>> findAll(){
-		List<Category> list = service.findAll();
+	public ResponseEntity<List<CategoryDTO>> findAll(){
+		List<CategoryDTO> list = service.findAll();
 		return ResponseEntity.ok(list);
 	}
+
+	@GetMapping(value = "{id}")
+	public ResponseEntity<CategoryDTO> findById(@PathVariable Long id){
+		CategoryDTO dto = service.findById(id);
+		return ResponseEntity.ok().body(dto);
+	}
+
 
 }
--- dscatalog/backend/dscatalog/dscatalog/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ dscatalog/backend/dscatalog/dscatalog/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -1,7 +1,11 @@
 package com.devsuperior.dscatalog.services;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
+import com.devsuperior.dscatalog.dto.CategoryDTO;
+import com.devsuperior.dscatalog.exceptions.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,13 +15,20 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class CategoryService {
-	
+
 	@Autowired
 	private CategoryRepository repository;
 
 	@Transactional(readOnly = true)
-	public List<Category> findAll(){
-		return repository.findAll();
+	public List<CategoryDTO> findAll() {
+		List<Category> list = repository.findAll();
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 	}
 
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Categoria não encontrada"));
+		return new CategoryDTO(entity);
+	}
 }
