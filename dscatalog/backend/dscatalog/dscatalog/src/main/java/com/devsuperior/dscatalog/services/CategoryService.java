6f21cd6439b8fa14cd676fc46f4843ee14d6c7e1
Marcos Vinício Fernandes <marcosviniciohd@hotmail.com>
2022-11-22 23:05:51 -0300

    POST new category

--- dscatalog/backend/dscatalog/dscatalog/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ dscatalog/backend/dscatalog/dscatalog/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -7,11 +7,14 @@ import java.util.stream.Collectors;
 import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.exceptions.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 public class CategoryService {
@@ -29,6 +32,14 @@ public class CategoryService {
 	public CategoryDTO findById(Long id) {
 		Optional<Category> obj = repository.findById(id);
 		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Categoria não encontrada"));
+		return new CategoryDTO(entity);
+	}
+
+	@Transactional
+	public CategoryDTO insert(CategoryDTO dto) {
+		Category entity = new Category();
+		entity.setName(dto.getName());
+		entity = repository.save(entity);
 		return new CategoryDTO(entity);
 	}
 }
